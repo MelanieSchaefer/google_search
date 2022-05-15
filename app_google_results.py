44c8150a5fda55b6b99faadc92198ef27786c119
MelanieSchaefer <34131599+MelanieSchaefer@users.noreply.github.com>
2022-05-15 23:24:00 +0200

    Update app_google_results.py

--- app_google_results.py
+++ app_google_results.py
@@ -1,66 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 12 09:56:12 2022
+Created on Fri May 13 10:33:43 2022
 @author: Melanie
 """
 
-# Reads data from incoming POST-requests
-# Checks authentification of user
-# Finds the first 10 results in Google by given keyword
-# Startfunction main(request) will be called by endpoint POST-request handler
+# Creates an endpoint to handle GET- and POST-requests
+# Handles authorization
 
-import json
-from tld import get_tld
+import app_google_results
+import jwt
+import datetime
+from flask import Flask, jsonify, request, make_response
+from flask import request
+from functools import wraps
 
-# Extracts username, password and keyword from body -> form-data from POST-request
-# Input: unmodified Post-request
-def read_Post_request(request): 
-    #read_username = request.form.get('username')
-    #read_password = request.form.get('password')
-    read_keyword = request.form.get('keyword')  
-    return read_keyword
+# Create a flask (web) application
+app = Flask(__name__)
 
-# Check authentification by comparing given username and password with 
-# fix username and password 
-#def authentification(username, password):
-#    if username == 'East': 
-#        if password == 'West':
-#            return True
+app.config['SECRET_KEY'] = 'secretkey'
 
-# Searching for the first 10 google results of given keyword
-# Input: String keyword
-# Output: JSON Scheme: {'results': [{result_ID=1, url, tld}, ..., {result_ID=10, url, tld}]}
-    # result_ID: position from top (1) to buttom (10)
-    # url: google result website link
-    # tld: top level domain
-def google_results(keyword): 
-    try:
-        from googlesearch import search
-    except ImportError:
-        print("No module named 'google' found")
-
-    current_position = 1
-    object_list = []
-    for j in search(keyword, tld="co.in", num=10, stop=10, pause=2):
-        x = {'result_ID' : current_position, 'url' : j, 
-             'tld' : get_tld(j)}
-        object_list.append(x)
-        current_position = current_position + 1
+def token_required(f): 
+    @wraps(f)
+    def decorated(*args, **kwargs): 
+        token = request.args.get('Token') 
+        if not token:
+            return jsonify({'message': 'Token is missing!', 'request' : request}), 401
         
-    outcome = {}
-    outcome.update({"results" : object_list})   
-    jsonStr = json.dumps(outcome)
-    return jsonStr 
+        try: 
+            jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        except: 
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(*args, **kwargs)
+    return decorated
 
-# Reads data from Post-request by method read_Post_request(request)
-# Checks authentification of user by method authentification(read_username,read_password)
-# Searchs keyword in Googl by method google_results(read_keyword)
-# Input: POST-request
-# Return: Search results in jsonStr (JSON String)  
-def main(request): 
-    read_keyword = read_Post_request(request)
-    if (not read_keyword):
-        return "Data is missing"
-    else:  
-        jsonStr = google_results(read_keyword)
-        return jsonStr
+@app.route('/login')
+def login(): 
+    auth = request.authorization
+    
+    if auth and auth.password == 'password': 
+        token = jwt.encode({'user': auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=2)}, app.config['SECRET_KEY'], algorithm='HS256')
+        return jsonify({'token': token})
+    return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm=!Login required"'})
+
+# Process incoming GET- and POST-requests
+# IP: 127.0.0.1, Port: 5000  
+@app.route('/', methods=['GET', 'POST'])
+@token_required
+def request_handler():
+    if request.method == 'POST':
+        google_results = app_google_results.main(request)
+        return google_results 
+    elif request.method == 'GET': 
+        return jsonify({'message': 'Welcome! This was a GET-request'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)  
